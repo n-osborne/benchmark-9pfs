ead74496aa07a832cbac32b27dea586e794873bc
Nicolas Osborne <nicolas.osborne@tarides.com>
2025-11-28 17:36:35 +0100

    Read the entire file

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
@@ -9,30 +10,41 @@
 #endif
 
 int main () {
-  int fd ;
+  int fd, i;
   char buf[BUF_SIZE];
-  // struct dirent *pDirent;
-  // DIR *pDir;
+  char *path;
+  struct dirent *pDirent;
+  DIR *pDir;
 
   printf ("Let's try some stuff!\n\n");
 
   // if (getcwd (buf, BUF_SIZE) == NULL) { perror ("getcwd () failed:"); }
   // printf ("current working directory: %s\n", buf);
 
-  // pDir = opendir ("/");
-  // if (pDir == NULL) { perror ("opendir (\"/\") failed:"); }
+  pDir = opendir ("/");
+  if (pDir == NULL) { perror ("opendir (\"/\") failed:"); }
 
-  // pDirent = readdir (pDir);
-  // while (pDirent != NULL) {
-  //   printf ("%s\n", pDirent->d_name);
-  //   pDirent = readdir (pDir);
-  // }
+  pDirent = readdir (pDir);
+  while (pDirent != NULL) {
+    printf ("%s\n", pDirent->d_name);
+    pDirent = readdir (pDir);
+  }
 
-  fd = open ("./data", O_RDONLY, 0777);
-  if (fd == -1) { perror ("open (\"./data\") failed:"); };
+  printf ("%s\n", path);
+  fd = open (path, O_RDONLY, 0777);
+  if (fd == -1) {
+    perror ("open () failed:");
+    exit(EXIT_FAILURE);
+  };
 
-  if (0 > read (fd, buf, BUF_SIZE)) { close (fd); perror ("read () failed:"); }
-  printf ("%s\n", buf);
+  while (i = read (fd, buf, BUF_SIZE)) {
+    if (i == -1) {
+      close (fd);
+      perror ("read () failed:");
+      exit(EXIT_FAILURE);
+    }
+    printf ("%s\n", buf);
+  }
 
   close (fd);
 
