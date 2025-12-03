56034898b2a043e1baab239ee6e77a3bf9fb8fbf
Nicolas Osborne <nicolas.osborne@tarides.com>
2025-12-03 13:14:40 +0100

    Store time spend to read a file
    Time is written in `shared/results.txt`. Each entry is appended at the
    end of the file so that we can run multiple times the unikernel and
    compute some stats.

--- main.c
+++ main.c
@@ -3,50 +3,73 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-#include <dirent.h>
+#include <time.h>
 
 #ifndef BUF_SIZE
 #define BUF_SIZE 100
 #endif
 
+#ifndef PATH
+#define PATH "data"
+#endif
+
+#ifndef RESULTS
+#define RESULTS "results.txt"
+#endif
+
 int main () {
-  int fd, i;
+
+  int fd, res, time, length, start, end;
   char buf[BUF_SIZE];
-  char *path;
-  struct dirent *pDirent;
-  DIR *pDir;
+  char *str;
+  struct timespec tspec;
 
-  printf ("Let's try some stuff!\n\n");
+  printf("Let's try some stuff!\n\n");
 
-  // if (getcwd (buf, BUF_SIZE) == NULL) { perror ("getcwd () failed:"); }
-  // printf ("current working directory: %s\n", buf);
-
-  pDir = opendir ("/");
-  if (pDir == NULL) { perror ("opendir (\"/\") failed:"); }
-
-  pDirent = readdir (pDir);
-  while (pDirent != NULL) {
-    printf ("%s\n", pDirent->d_name);
-    pDirent = readdir (pDir);
-  }
-
-  printf ("%s\n", path);
-  fd = open (path, O_RDONLY, 0777);
+  fd = open(PATH, O_RDONLY, 0777);
   if (fd == -1) {
-    perror ("open () failed:");
+    perror("open () failed:");
     exit(EXIT_FAILURE);
   };
 
-  while (i = read (fd, buf, BUF_SIZE)) {
-    if (i == -1) {
-      close (fd);
-      perror ("read () failed:");
+  printf("Reading %s...\n", PATH);
+
+  res = clock_gettime(CLOCK_MONOTONIC, &tspec);
+  if (res == -1) { perror("clock_gettime () failed:"); }
+  start = tspec.tv_nsec / 1000;
+
+  while (res = read(fd, buf, BUF_SIZE)) {
+    if (res == -1) {
+      close(fd);
+      perror("read () failed:");
       exit(EXIT_FAILURE);
     }
-    printf ("%s\n", buf);
   }
 
-  close (fd);
+  res = clock_gettime(CLOCK_MONOTONIC, &tspec);
+  if (res == -1) { perror ("clock_gettime () failed:"); }
+  end = tspec.tv_nsec / 1000;
 
+  close(fd);
+
+  fd = open(RESULTS, O_WRONLY | O_CREAT | O_APPEND, 0777);
+  if (fd == -1) {
+    perror("open () failed:");
+    exit(EXIT_FAILURE);
+  };
+
+  time = end - start;
+  length = snprintf(NULL, 0, "%d\n", time);
+  str = malloc((1 + length) * sizeof(char));
+
+  res = sprintf(str, "%d\n", time);
+  if (res < 0) { perror("sprintf() failed:"); exit(EXIT_FAILURE); }
+
+  res = write(fd, str, length + 1);
+  free(str);
+  close(fd);
+  if (res == -1) { perror("write() failed:"); exit(EXIT_FAILURE); }
+
+  fflush(stdout);
   return 0;
 }
