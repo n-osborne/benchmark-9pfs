d57da03e92a4f0f983dfc4c806b4018474610af0
Nicolas Osborne <nicolas.osborne@tarides.com>
2026-01-05 11:47:54 +0100

    Make unikernel take mode argument
    'read' is for benchmarking read and is the only one defined so far.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <time.h>
+#include <string.h>
 
 #ifndef PATH
 #define PATH "data"
@@ -26,8 +27,9 @@ long benchmark_read(int fd, int buf_size, char* buf) {
   int res;
   struct timespec start, end;
 
-  printf("Reading %s...\n", PATH);
+  printf("Reading data from %s...\n", PATH);
 
+  // start the clock
   res = clock_gettime(CLOCK_MONOTONIC, &start);
   if (res == -1) {
     free(buf);
@@ -35,7 +37,8 @@ long benchmark_read(int fd, int buf_size, char* buf) {
     perror("clock_gettime () failed:");
   }
 
-  while (res = read(fd, buf, buf_size)) {
+  // read data
+  while ((res = read(fd, buf, buf_size))) {
     if (res == -1) {
       free(buf);
       close(fd);
@@ -44,6 +47,7 @@ long benchmark_read(int fd, int buf_size, char* buf) {
     }
   }
 
+  // stop the clock
   res = clock_gettime(CLOCK_MONOTONIC, &end);
   if (res == -1) {
     free(buf);
@@ -51,37 +55,57 @@ long benchmark_read(int fd, int buf_size, char* buf) {
     perror ("clock_gettime () failed:");
   }
 
-  free(buf);
-  close(fd);
-
   return compute_time_ms(&start, &end);
 }
 
 int main(int argc, char **argv) {
 
-  int fd, res, length, buf_size;
+  int fd, res, length, buf_size, data_size;
   long time;
   char *buf, *str;
-  struct timespec start, end;
 
-  printf("Let's try some stuff!\n\n");
+  printf("Let's bench some stuff!\n\n");
 
-  // Not the best error handling...
-  if (argc < 1) { exit(EXIT_FAILURE); }
+  // Read buffer size from call
+  if (argc < 2) { exit(EXIT_FAILURE); }
   buf_size = atoi(argv[1]);
   if (buf_size == 0) { exit(EXIT_FAILURE); }
 
+  // Allocate buffer
   buf = malloc(buf_size);
   if (buf == NULL) { exit(EXIT_FAILURE); }
 
-  fd = open(PATH, O_RDONLY, 0777);
+  // Read data size if set on call
+  if (argc > 3) {
+    data_size = atoi(argv[3]);
+    if (data_size == 0) { exit(EXIT_FAILURE); }
+  }
+
+  // Determine path depending on the mode that is given from call
+  if (strcmp(argv[2], "read") == 0) {
+    fd = open(PATH, O_RDONLY, 0777);
+  }
+  else if (strcmp(argv[2], "write") == 0) {
+    fd = open(PATH, O_WRONLY | O_CREAT, 0777);
+  }
+  else { exit(EXIT_FAILURE); }
+
   if (fd == -1) {
     free(buf);
     perror("open () failed:");
     exit(EXIT_FAILURE);
   };
 
-  time = benchmark_read(fd, buf_size, buf);
+  // Run benchmark according the mode given on call
+  if (strcmp(argv[2], "read") == 0) {
+    time = benchmark_read(fd, buf_size, buf);
+  }
+  else { exit(EXIT_FAILURE); }
+
+  free(buf);
+  close(fd);
+
+  // compute length of the string representing benchmark timing
   length = snprintf(NULL, 0, "%ld\n", time);
   if (length < 0) {
     perror("snprintf() failed:");
@@ -90,6 +114,7 @@ int main(int argc, char **argv) {
   str = malloc((1 + length) * sizeof(char));
   if (buf == NULL) { exit(EXIT_FAILURE); }
 
+  // open result file for appending new benchmark result
   fd = open(RESULTS, O_WRONLY | O_CREAT | O_APPEND, 0777);
   if (fd == -1) {
     free(str);
@@ -97,6 +122,7 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   };
 
+  // prepare result string
   res = sprintf(str, "%ld\n", time);
   if (res < 0) {
     free(str);
@@ -104,6 +130,7 @@ int main(int argc, char **argv) {
     perror("sprintf() failed:"); exit(EXIT_FAILURE);
   }
 
+  // write new benchmark result in result file
   res = write(fd, str, length + 1);
   free(str);
   close(fd);
--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ mkdir -p $SHARED
 mkdir -p $OUTDIR
 rm ${OUTDIR}/*
 
-for size in 1 3 5 7 9
+for size in 1
 do
   BUFSIZE=${size}"00000"
   printf "buffer size: %s\n" $BUFSIZE
@@ -18,16 +18,16 @@ do
 # it
 > $RESULTS
 
-for _ in {0..99}
-do
+# for _ in {0..99}
+# do
 dd if=/dev/urandom of=$DATA bs=64M count=16 iflag=fullblock
 
 qemu-system-x86_64 -cpu host --enable-kvm -nographic -m 1G \
   -nodefaults -serial stdio -kernel .unikraft/build/bob_qemu-x86_64 \
-  -append "vfs.fstab=[ \"fs1:/:9pfs:::mkmp\" ] -- $BUFSIZE" \
+  -append "vfs.fstab=[ \"fs1:/:9pfs:::mkmp\" ] -- $BUFSIZE read" \
   -virtfs local,path=$SHARED,mount_tag=fs1,security_model=passthrough
 
-done
+# done
 
 sed -i 's/\x00//g' $RESULTS
 sed -i '/^\s*$/d' $RESULTS
